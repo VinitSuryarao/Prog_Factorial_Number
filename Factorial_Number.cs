2432e595b499202f303cd7537e86cf47a2301611
Vinit Suryarao <suryarao.vinit@hotmail.com>
2021-10-03 19:00:04 +0530

    Add files via upload

---
+++ Factorial_Number.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+    class Program
+    {
+        //   Factorial Number
+        //   Number = 5
+        //   Factorial = 5 * 4 * 3 * 2 * 1
+
+        static void Main(string[] args)
+        {
+            int n, fact;
+            n = 5;
+            fact = n;
+
+            for (int i= n-1; i>=1; i--)
+            {
+                fact = fact * i;
+            }
+
+            Console.WriteLine("Factorial Number is " + fact.ToString());
+            Console.ReadLine();
+        }
+    }
+
+
